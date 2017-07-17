320065a779c77c1595e554a955f53e3470fdf5f0
encadyma <encadyma@gmail.com>
2017-07-17 12:29:55 -0700

    Completed error handling on frontend site (basic)

--- src/app/login-view/login-view.component.ts
+++ src/app/login-view/login-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { AuthService } from '../_services/auth.service';
+import { API_BASE } from '../models/api';
 
 @Component({
   selector: 'app-login-view',
@@ -11,11 +12,20 @@ import { AuthService } from '../_services/auth.service';
 export class LoginViewComponent implements OnInit {
 
     authStatus: number;
+    errors = {
+        successLogin: [100],
+        successLogout: [200],
+        notLoggedIn: [101],
+        generalError: [102, 103, 104, 105, 106, 107, 108, 109, 110]
+    };
 
     constructor(private authService: AuthService, private route: ActivatedRoute) { }
 
     ngOnInit() {
         this.authStatus = parseInt(this.route.snapshot.queryParamMap.get('authstatus'), undefined);
+        if (!this.authStatus || this.authStatus === 101) {
+            window.location.href = `${API_BASE}/users/login/generate`;
+        }
     }
 
 }
