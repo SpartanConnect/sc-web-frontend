fd15d622bda33258e81db5318504e02d8e6b9efb
encadyma <encadyma@gmail.com>
2017-07-18 10:32:28 -0700

    Modified requests to send with credentials

--- src/app/models/api.ts
+++ src/app/models/api.ts
@@ -1,10 +1,13 @@
-import { Headers, Http } from '@angular/http';
+import { Headers, Http, RequestOptions, RequestOptionsArgs } from '@angular/http';
 import { environment } from '../../environments/environment';
 
 export const API_BASE = environment.apiUrl;
 
-export function httpHandler(http, apiLink) {
-    return http.get(apiLink)
+export function httpHandler(http, apiLink, queryData = {}) {
+    const httpOptions: RequestOptionsArgs = new RequestOptions({
+        withCredentials: true
+    });
+    return http.get(apiLink, httpOptions)
         .toPromise()
         .then((data) => {
             return data.json();
