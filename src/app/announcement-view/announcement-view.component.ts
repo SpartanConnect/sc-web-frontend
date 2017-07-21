6b0fc008ba237eeba0296ffcd2f8b1abd2bf7311
encadyma <encadyma@gmail.com>
2017-07-20 22:49:16 -0700

    Enabled view protection -- only authors of their own posts or approved posts can be viewed normally

--- src/app/announcement-view/announcement-view.component.ts
+++ src/app/announcement-view/announcement-view.component.ts
@@ -4,30 +4,51 @@ import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
 
 import { AnnouncementsService } from '../_services/announcements.service';
+import { AuthService } from '../_services/auth.service';
 import { Announcement } from '../models/announcement';
 
 @Component({
-  selector: 'app-announcement-view',
-  templateUrl: './announcement-view.component.html',
-  styleUrls: ['./announcement-view.component.scss']
+    selector: 'app-announcement-view',
+    templateUrl: './announcement-view.component.html',
+    styleUrls: ['./announcement-view.component.scss']
 })
 export class AnnouncementViewComponent implements OnInit {
 
-  announcement: Announcement;
-  loading: boolean;
-  isArchived: boolean;
+    announcement: Announcement;
+    loading: boolean;
+    isArchived: boolean;
+    isHidden: boolean;
 
-  constructor(private announcementService: AnnouncementsService, private route: ActivatedRoute, private router: Router) { }
+    constructor(private announcementService: AnnouncementsService, private route: ActivatedRoute,
+        private router: Router, private authService: AuthService) { }
 
-  ngOnInit() {
-      window.scrollTo(0, 0);
+    ngOnInit() {
+        window.scrollTo(0, 0);
 
-      this.loading = true;
-      // tslint:disable-next-line:radix
-      this.announcementService.getAnnouncementById(parseInt(this.route.snapshot.paramMap.get('id'))).then((data) => {
-          this.announcement = data;
-          this.loading = false;
-      })
-  }
+        this.loading = true;
+        this.isArchived = false;
+        this.isHidden = false;
+        // tslint:disable-next-line:radix
+        this.announcementService.getAnnouncementById(parseInt(this.route.snapshot.paramMap.get('id'))).then((data) => {
+            if (!data) {
+                // No announcement data returned.
+                this.announcement = null;
+                this.loading = false;
+            } else if (data.status === 1) {
+                // If the announcement is generally approved
+                this.announcement = data;
+                this.loading = false;
+            } else if (data.creator.id === this.authService.currentUser.id) {
+                // If the announcement belongs to the currently logged in user.
+                this.announcement = data;
+                this.loading = false;
+                this.isHidden = true;
+            } else {
+                // Can't catch error.
+                this.announcement = null;
+                this.loading = false;
+            }
+        });
+    }
 
 }
