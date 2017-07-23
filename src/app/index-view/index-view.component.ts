17e8485896adcc38a31af66a92f7863093f18c5b
encadyma <encadyma@gmail.com>
2017-07-22 23:11:05 -0700

    Fixed index page formatting and bug with sorting

--- src/app/index-view/index-view.component.ts
+++ src/app/index-view/index-view.component.ts
@@ -7,47 +7,48 @@ import { Announcement } from '../_models/announcement';
 import { Tag } from '../_models/tag';
 
 @Component({
-  selector: 'app-index-view',
-  templateUrl: './index-view.component.html',
-  styleUrls: ['./index-view.component.scss']
+    selector: 'app-index-view',
+    templateUrl: './index-view.component.html',
+    styleUrls: ['./index-view.component.scss']
 })
 export class IndexViewComponent implements OnInit {
 
-  announcements: Announcement[] = [];             // All announcements
-  announcementsSliced: Announcement[] = [];       // The header's first eight announcements
-  announcementHighlight: Announcement = null;     // The first announcement
-  categories: Tag[] = [];                         // All categories
-  categoryFilter = 0;                             // The current category selected.
-  sortedAnnouncements = {};                       // An assoc object.
+    announcements: Announcement[] = [];             // All announcements
+    announcementsSliced: Announcement[] = [];       // The header's first eight announcements
+    announcementHighlight: Announcement = null;     // The first announcement
+    categories: Tag[] = [];                         // All categories
+    categoryFilter = 0;                             // The current category selected.
+    sortedAnnouncements = {};                       // An assoc object.
 
-  constructor(public snackBar: MdSnackBar, private announcementsService: AnnouncementsService, private tagsService: TagsService) { }
+    constructor(public snackBar: MdSnackBar, private announcementsService: AnnouncementsService, private tagsService: TagsService) { }
 
-  switchCategoryFilter(id: number) {
-      this.categoryFilter = id;
-  }
+    switchCategoryFilter(id: number) {
+        this.categoryFilter = id;
+    }
 
-  ngOnInit() {
-      const announcementPromise = this.announcementsService.getCurrentAnnouncements().then((data) => {
-          this.announcements = data;
-          this.announcementsSliced = this.announcements.slice(0, 8);
-          this.announcementHighlight = this.announcements[0];
-      });
-      const tagsPromise = this.tagsService.getCategories().then((data) => {
-          this.categories = data;
-          this.categories.map((category) => {
-              this.sortedAnnouncements[category.slug] = [];
-          });
-      });
+    ngOnInit() {
+        const announcementPromise = this.announcementsService.getCurrentAnnouncements().then((data) => {
+            this.announcements = data;
+            this.announcementsSliced = this.announcements.slice(0, 8);
+            this.announcementHighlight = this.announcements[0];
+        });
+        const tagsPromise = this.tagsService.getCategories().then((data) => {
+            this.categories = data;
+            this.categories.map((category) => {
+                this.sortedAnnouncements[category.slug] = [];
+            });
+        });
 
-      Promise.all([announcementPromise, tagsPromise]).then(() => {
-          this.announcements.map((announcement) => {
-              announcement.tags.map((tag) => {
-                  if (tag.parentId !== null) { return false; };
-                  this.sortedAnnouncements[tag.slug].push(announcement);
-                  return true;
-              });
-          });
-      });
-  }
+        Promise.all([announcementPromise, tagsPromise]).then(() => {
+            this.announcements.map((announcement) => {
+                announcement.tags.map((tag) => {
+                    if (tag.parentId !== null) { return false; };
+                    if (!Array.isArray(this.sortedAnnouncements[tag.slug])) { this.sortedAnnouncements[tag.slug] = []; }
+                    this.sortedAnnouncements[tag.slug].push(announcement);
+                    return true;
+                });
+            });
+        });
+    }
 
 }
