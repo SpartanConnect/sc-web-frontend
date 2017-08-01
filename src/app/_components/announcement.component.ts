f9a2bc03eb4f620dd915b55b29ba9439a573568a
encadyma <encadyma@gmail.com>
2017-08-01 16:39:47 -0700

    Archival of announcements work

--- src/app/_components/announcement.component.ts
+++ src/app/_components/announcement.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
+import { TdDialogService } from '@covalent/core';
 
+import { AnnouncementsService } from '../_services/announcements.service';
 import { TagsService } from '../_services/tags.service';
 import { Announcement } from '../_models/announcement';
 
@@ -36,7 +38,9 @@ export class AnnouncementComponent implements OnInit {
         category: 0
     }
 
-    constructor(private tagsService: TagsService) { }
+    constructor(
+        private tagsService: TagsService, private dialogService: TdDialogService,
+        private announcementsService: AnnouncementsService, private ref: ChangeDetectorRef) { }
 
     // Gets the latest action and sets it in the announcement footer
     getLatestAction() {
@@ -76,10 +80,56 @@ export class AnnouncementComponent implements OnInit {
 
     submitForEditing() {
         // send post request
+        this.isEditing = false;
+        this.announcement.title = this.editedAnnouncement.title;
+        this.announcement.description = this.editedAnnouncement.description;
+        this.announcement.timeEdited = new Date();
+        this.getLatestAction();
     }
 
-    archiveAnnouncement() {
-        this.isHidden = true;
+    archiveAnnouncement(archive = true) {
+        if (archive) {
+            this.dialogService.openConfirm({
+                message: ((this.announcement.status === 1) ?
+                    'Removing an announcement will remove its approval status. Remove?' :
+                    `Are you sure you want to remove this announcement? Once an announcement has been removed,
+                    an administrator cannot approve it until it has been resubmitted.`),
+                disableClose: true,
+                title: 'Confirm Removal',
+                cancelButton: 'NO',
+                acceptButton: 'YES'
+            }).afterClosed().subscribe((value: boolean) => {
+                if (value) {
+                    this.announcementsService.setAnnouncementStatus(
+                        this.announcement.id,
+                        3
+                    ).then((r) => {
+                        if (r.success) {
+                            this.announcement.status = 3;
+                        }
+                    });
+                }
+            });
+        } else {
+            this.dialogService.openConfirm({
+                message: 'Are you sure you want to resubmit this announcement for approval?',
+                disableClose: true,
+                title: 'Confirm Resubmission',
+                cancelButton: 'NO',
+                acceptButton: 'YES'
+            }).afterClosed().subscribe((value: boolean) => {
+                if (value) {
+                    this.announcementsService.setAnnouncementStatus(
+                        this.announcement.id,
+                        0
+                    ).then((r) => {
+                        if (r.success) {
+                            this.announcement.status = 0;
+                        }
+                    });
+                }
+            });
+        }
     }
 
     ngOnInit() {
