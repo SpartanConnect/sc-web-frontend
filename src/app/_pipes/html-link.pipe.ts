e4d5d762406f797dd19e18f138ebb7e772569497
encadyma <encadyma@gmail.com>
2017-08-19 12:27:29 -0700

    Add http:// to links without them

--- src/app/_pipes/html-link.pipe.ts
+++ src/app/_pipes/html-link.pipe.ts
@@ -6,6 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class HtmlLinkPipe implements PipeTransform {
     transform(value: string, ...args: any[]): string {
         const urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-        return value.replace(urlRegex, `<a href="$&">$&</a>`);
+        return value.replace(urlRegex, `<a href="` + (/^https?:\/\//i.test(value) ? '' : 'http://') + `$&">$&</a>`);
     }
 }
